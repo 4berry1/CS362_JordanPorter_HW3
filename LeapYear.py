a564ffda1f68708c9a9b89f39fba602052c8d681
Jordan <4berry1@gmail.com>
2021-04-25 16:21:12 -0700

    add error handling to leap year
    added error handling to leap year if a value that is less than 0 or not an int is entered.

--- LeapYear.py
+++ LeapYear.py
@@ -1,17 +1,25 @@
 
-# run this program by using python Jordan_porter_hw1.py on the osu server
+# run this program by using python LeapYear.py on the osu server
 
 def main() :
-	year = input("enter the year to check for leap year : ")
 	
-	if  year % 4 > 0 :
-		print year, " is not a leap year"
-	elif year % 100 > 0 :
-		print year, "is a leap year"
-	elif year % 400 == 0 :
-		print year, "is a leap year"
-	else:	
-		print year, "is not a leap year"
+	while(1):
+		try:
+			year = int(input ("enter the year to check for leap year : "))
 
+			if(year < 0):
+				year = "sadfdsfd"
+	
+			if  year % 4 > 0 :
+				print (year), " is not a leap year"
+			elif year % 100 > 0 :
+				print (year), "is a leap year"
+			elif year % 400 == 0 :
+				print (year), "is a leap year"
+			else:	
+				print (year), "is not a leap year"
+			return
+		except:
+			print "error with input enter an integer > 0"
 
 main()
